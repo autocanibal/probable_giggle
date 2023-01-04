c6c4bf9dac4dad08b0cb2ec5a8cfdacf7bbf0325
Monde <mondzzmkhize@gmail.com>
2023-01-04 22:35:06 +0200

    remove mainactivity content

--- app/src/main/java/org/mkhizem/probable_giggle/MainActivity.kt
+++ app/src/main/java/org/mkhizem/probable_giggle/MainActivity.kt
@@ -16,15 +16,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProbableGiggleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
         }
     }
 }
